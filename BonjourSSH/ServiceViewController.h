555a3706a21406ecc57c8201eaa0154d3c598646
Eugene Hp <eugene.hp2012@gmail.com>
2014-03-26 07:25:22 +0100

    initial Bonjour services discovery

---
+++ BonjourSSH/ServiceViewController.h
@@ -0,0 +1,16 @@
+//
+//  BonjourViewController.h
+//  BonjourSSH
+//
+//  Created by Eugene on 3/26/14.
+//  Copyright (c) 2014 www.eugenehp.tk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceViewController : UIViewController <NSNetServiceBrowserDelegate, NSNetServiceDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property(nonatomic, retain) NSString* serviceName;
+
+@end
